4ed5dbbdcd29aa7e63facdd574b93d9b89f46825
ikemura <banbara.work@gmail.com>
2018-05-28 19:05:05 +0900

    update infoFragment

--- app/src/main/java/ikemura/com/simanchu_takara_sagashi_android/ui/info/InfoFragment.kt
+++ app/src/main/java/ikemura/com/simanchu_takara_sagashi_android/ui/info/InfoFragment.kt
@@ -5,8 +5,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ikemura.com.simanchu_takara_sagashi_android.BuildConfig
 import ikemura.com.simanchu_takara_sagashi_android.R
 import ikemura.com.simanchu_takara_sagashi_android.ui.main.main.MainViewModel
+import kotlinx.android.synthetic.main.info_fragment.*
 
 class InfoFragment : Fragment() {
 
@@ -23,18 +25,7 @@ class InfoFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-//        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-//        val text = viewModel.data.value
-//        title.text = text
-    }
 
-    override fun onResume() {
-        super.onResume()
-//        topImage.resume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-//        topImage.pause()
+        version_name.text = "Version ${BuildConfig.VERSION_NAME}"
     }
 }
