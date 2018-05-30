2d4bb9bf4866834409e907af220b20d740b61689
ikemura <banbara.work@gmail.com>
2018-05-30 23:12:28 +0900

    add Navigator

--- app/src/main/java/ikemura/com/simanchu_takara_sagashi_android/ui/main/MainFragment.kt
+++ app/src/main/java/ikemura/com/simanchu_takara_sagashi_android/ui/main/MainFragment.kt
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import ikemura.com.simanchu_takara_sagashi_android.R
+import ikemura.com.simanchu_takara_sagashi_android.R.id.topImage
 import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
@@ -25,8 +27,24 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        val text = viewModel.data.value
-        title.text = text
+//        val text = viewModel.data.value
+//        title.text = text
+        setClickEvent()
+    }
+
+    private fun setClickEvent() {
+        //カード1のクリック処理
+        include_card1.setOnClickListener { view ->
+            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_detailFragment)
+        }
+        //カード2のクリック処理
+        include_card2.setOnClickListener { view ->
+            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_detailFragment)
+        }
+        //カード3のクリック処理
+        include_card3.setOnClickListener { view ->
+            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_detailFragment)
+        }
     }
 
     override fun onResume() {
