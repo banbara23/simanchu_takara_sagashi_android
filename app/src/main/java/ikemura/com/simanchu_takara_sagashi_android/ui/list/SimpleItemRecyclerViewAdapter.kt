ae9972ac19b72ba77aa329a47e4ccc1d1538701d
ikemura <k_ikemura@yumemi.co.jp>
2018-06-01 16:11:34 +0900

    picasso追加、dependencies整理

---
+++ app/src/main/java/ikemura/com/simanchu_takara_sagashi_android/ui/list/SimpleItemRecyclerViewAdapter.kt
@@ -0,0 +1,73 @@
+package ikemura.com.simanchu_takara_sagashi_android.ui.list
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import ikemura.com.simanchu_takara_sagashi_android.R
+import ikemura.com.simanchu_takara_sagashi_android.model.Spot
+import ikemura.com.simanchu_takara_sagashi_android.ui.detail.SpotDetailActivity
+import ikemura.com.simanchu_takara_sagashi_android.ui.detail.SpotDetailFragment
+import kotlinx.android.synthetic.main.item_list_content.view.*
+
+class SimpleItemRecyclerViewAdapter(private val parentActivity: SpotListActivity,
+                                    private val spots: List<Spot>,
+                                    private val twoPane: Boolean) :
+        RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
+
+    private val onClickListener: View.OnClickListener
+
+    init {
+        onClickListener = View.OnClickListener { v ->
+            val item = v.tag as Spot
+            if (twoPane) {
+                val fragment = SpotDetailFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(SpotDetailFragment.ARG_ITEM_ID, item.id)
+                    }
+                }
+                parentActivity.supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.item_detail_container, fragment)
+                        .commit()
+            } else {
+                val intent = Intent(v.context, SpotDetailActivity::class.java).apply {
+                    putExtra(SpotDetailFragment.ARG_ITEM_ID, item.id)
+                }
+                v.context.startActivity(intent)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_list_content, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = spots[position]
+//        Picasso.get().load("1").into(holder.imageView)
+        holder.nameView.text = item.name
+        holder.levelView.text = item.level
+        holder.placeView.text = item.place
+
+        with(holder.itemView) {
+            tag = item
+            setOnClickListener(onClickListener)
+        }
+    }
+
+    override fun getItemCount() = spots.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imageView: ImageView = view.image
+        val nameView: TextView = view.name
+        val levelView: TextView = view.level
+        val placeView: TextView = view.place
+    }
+}
--- app/src/main/java/ikemura/com/simanchu_takara_sagashi_android/ui/list/SpotListActivity.kt
+++ app/src/main/java/ikemura/com/simanchu_takara_sagashi_android/ui/list/SpotListActivity.kt
@@ -1,21 +1,13 @@
 package ikemura.com.simanchu_takara_sagashi_android.ui.list
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import ikemura.com.simanchu_takara_sagashi_android.R
 import ikemura.com.simanchu_takara_sagashi_android.SpotRepository
-import ikemura.com.simanchu_takara_sagashi_android.model.Spot
 import ikemura.com.simanchu_takara_sagashi_android.ui.detail.SpotDetailActivity
-import ikemura.com.simanchu_takara_sagashi_android.ui.detail.SpotDetailFragment
 import kotlinx.android.synthetic.main.activity_item_list.*
 import kotlinx.android.synthetic.main.item_list.*
-import kotlinx.android.synthetic.main.main_card.view.*
 
 /**
  * An activity representing a list of Pings. This activity
@@ -40,11 +32,6 @@ class SpotListActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         toolbar.title = title
 
-//        fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show()
-//        }
-
         if (item_detail_container != null) {
             // The detail container view will be present only in the
             // large-screen layouts (res/values-w900dp).
@@ -59,61 +46,5 @@ class SpotListActivity : AppCompatActivity() {
     private fun setupRecyclerView(recyclerView: RecyclerView) {
         val data = SpotRepository(this).fetchList()
         recyclerView.adapter = SimpleItemRecyclerViewAdapter(this, data.spots, twoPane)
-    }
-
-    class SimpleItemRecyclerViewAdapter(private val parentActivity: SpotListActivity,
-                                        private val spots: List<Spot>,
-                                        private val twoPane: Boolean) :
-            RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
-
-        private val onClickListener: View.OnClickListener
-
-        init {
-            onClickListener = View.OnClickListener { v ->
-                val item = v.tag as Spot
-                if (twoPane) {
-                    val fragment = SpotDetailFragment().apply {
-                        arguments = Bundle().apply {
-                            putString(SpotDetailFragment.ARG_ITEM_ID, item.id)
-                        }
-                    }
-                    parentActivity.supportFragmentManager
-                            .beginTransaction()
-                            .replace(R.id.item_detail_container, fragment)
-                            .commit()
-                } else {
-                    val intent = Intent(v.context, SpotDetailActivity::class.java).apply {
-                        putExtra(SpotDetailFragment.ARG_ITEM_ID, item.id)
-                    }
-                    v.context.startActivity(intent)
-                }
-            }
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_list_content, parent, false)
-            return ViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val item = spots[position]
-            holder.nameView.text = item.name
-            holder.levelView.text = item.level
-            holder.placeView.text = item.place
-
-            with(holder.itemView) {
-                tag = item
-                setOnClickListener(onClickListener)
-            }
-        }
-
-        override fun getItemCount() = spots.size
-
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val nameView: TextView = view.name
-            val levelView: TextView = view.level
-            val placeView: TextView = view.place
-        }
     }
 }
