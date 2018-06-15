17ddb798aefd3f2183af059ecfef23cbb1c796d7
ikemura <k_ikemura@yumemi.co.jp>
2018-06-16 01:37:00 +0900

    詳細に画像を仮表示してFABをハートにした

--- app/src/main/java/ikemura/com/simanchu_takara_sagashi_android/ui/detail/SpotDetailActivity.kt
+++ app/src/main/java/ikemura/com/simanchu_takara_sagashi_android/ui/detail/SpotDetailActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v4.app.NavUtils
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import ikemura.com.simanchu_takara_sagashi_android.R
@@ -18,13 +19,25 @@ import kotlinx.android.synthetic.main.activity_item_detail.*
  */
 class SpotDetailActivity : AppCompatActivity() {
 
+    private var isFavorite: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_detail)
         setSupportActionBar(detail_toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
+
+            isFavorite = !isFavorite
+            val drawable = if (isFavorite) {
+                R.drawable.ic_favorite_black_24dp
+            } else {
+                R.drawable.ic_favorite_border_black_24dp
+            }
+
+            fab.setImageDrawable(ContextCompat.getDrawable(this, drawable))
+
+            Snackbar.make(view, "お気に入り登録/解除しました", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
 
